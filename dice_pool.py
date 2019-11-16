81a9857c3f3f245d390421066dd8af62fb512d17
dGameBoy101b <44672112+dGameBoy101b@users.noreply.github.com>
2019-11-17 01:34:45 +0800

    Custom Range Prep
    Changed initialiser in Dice class to use a list of face results rather than a size.
    Changed string converter in Dice class to detect and produce basic, custom, and fudge ranges.
    Changed assertion tests in Dice class to correctly use changed initialliser.
    Added assertion tests in Dice class to test fudge dice and custom range dice.

--- dice_pool.py
+++ dice_pool.py
@@ -2,45 +2,60 @@ from roll_part_base import RollPart
 from random import randint
 
 class Dice(RollPart):
-    sep = 'd'
-    fudge = 'f'
-    def __init__(self, quant: int, size: int, neg: bool = False):
+    SEP = 'd'
+    FUDGE = 'f'
+    RANGE_START = '{'
+    RANGE_END = '}'
+    RANGE_SEP = ','
+    RANGE_BASE = 1
+    
+    def __init__(self, quant: int, faces: [int]):
         if not isinstance(quant, int):
             raise TypeError('\'quant\' must be an integer, not a ' + str(type(quant)))
-        if not isinstance(size, int):
-            raise TypeError('\'size\' must be and integer, not a ' + str(type(size)))
-        if not isinstance(neg, bool):
-            raise TypeError('\'neg\' must be a boolean, not a ' + str(type(neg)))
-        if size < 2:
-            raise ValueError('\'size\' must be greater than 1, not ' + str(size))
+        if not isinstance(faces, list):
+            raise TypeError('\'faces\' must be a list of integers, not a ' + str(type(faces)))
+        if len(faces) < 2:
+            raise ValueError('\'faces\' must be longer than 1, not ' + str(len(faces)))
+        for face in faces:
+            if not isinstance(face, int):
+                raise TypeError('\'faces\' must contain only integers, not a ' + str(type(face)))
         if quant < 1:
             raise ValueError('\'quant\' must be greater than 0, not ' + str(quant))
-        self.neg = neg
         self.quant = quant
-        self.size = size
+        self.faces = faces
         return
     
     def __repr__(self) -> str:
         '''Convert this dice pool into a machine executable string.
 
     Executing this string should create a copy of this dice pool.'''
-        return 'Dice('+str(self.quant)+','+str(self.size)+','+str(self.neg)+')'
+        return 'Dice('+str(self.quant)+', '+repr(self.faces)+')'
 
     def __str__(self) -> str:
         '''Convert this dice pool into a human readable string.
 
     This string should be able to be interpreted as an exact copy of this dice pool.'''
-        string = str(self.quant)+'d'+str(self.size)
-        if self.neg:
-            return Dice.NEG+string
+        string = str(self.quant) + Dice.SEP
+        if self.faces == list(range(Dice.RANGE_BASE, Dice.RANGE_BASE + len(self.faces))):
+            string = Dice.POS + string + str(len(self.faces) - 1 + Dice.RANGE_BASE)
+        elif self.faces == list(range((Dice.RANGE_BASE + len(self.faces) - 1) * -1, Dice.RANGE_BASE * -1 + 1)):
+            string = Dice.NEG + string + str(len(self.faces) - 1 + Dice.RANGE_BASE)
+        elif self.faces == [1,0,-1]:
+            string = Dice.POS + string + Dice.FUDGE
         else:
-            return Dice.POS+string
+            string = Dice.POS + string + Dice.RANGE_START
+            for i in range(len(self.faces)):
+                string += str(self.faces[i])
+                if i < len(self.faces) - 1:
+                    string += Dice.RANGE_SEP
+            string += Dice.RANGE_END
+        return string
 
     def __eq__(self, other) -> bool:
         '''Determine if this dice pool is equal to another dice pool.'''
         if not isinstance(other, Dice):
             raise TypeError('Dice can only be compared to other Dice, not ' + str(type(other)))
-        return self.quant == other.quant and self.size == other.size and self.neg == other.neg
+        return self.quant == other.quant and self.faces == other.faces
 
     def __ne__(self, other) -> bool:
         '''Determine whether this dice pool is not equal to another dice pool.'''
@@ -50,49 +65,44 @@ class Dice(RollPart):
         '''Calculate a random roll of this dice pool.'''
         random = [0] * self.quant
         for i in range(self.quant):
-            random[i] = randint(1, self.size)
-            if self.neg:
-                random[i] *= -1
+            random[i] = choose(self.faces)
         return random
 
     def min(self) -> [int]:
         '''Calculate the minimum roll of this dice pool.'''
-        minimum = [1] * self.quant
-        for i in range(self.quant):
-            if self.neg:
-                minimum[i] = -1 * self.size
-        return minimum
+        return [min(self.faces)] * self.quant
 
     def max(self) -> [int]:
         '''Calculate the mmaximum roll of this dice pool.'''
-        maximum = [0] * self.quant
-        for i in range(self.quant):
-            if self.neg:
-                maximum[i] = -1
-            else:
-                maximum[i] = self.size
-        return  maximum
+        return [max(self.faces)] * self.quant
 
     def avg(self) -> [float]:
         '''Calculate the average roll of this dice pool.'''
-        average = [(self.size + 1) / 2] * self.quant
-        for i in range(self.quant):
-            if self.neg:
-                average[i] *= -1 
-        return average
+        return [float(sum(self.faces)) / len(self.faces)] * self.quant
 
-assert Dice(1,2) == Dice(1,2, False)
-assert Dice(2,3) != Dice(3,2)
-assert Dice(1,2).quant == 1
-assert Dice(1,2).size == 2
-assert Dice(1,2).min() == [1]
-assert Dice(1,2).max() == [2]
-assert Dice(1,2).avg() == [1.5]
-assert repr(Dice(1,2)) == 'Dice(1,2,False)'
-assert str(Dice(1,2)) == '+1d2'
+assert Dice(1,list(range(1,2+1))) == Dice(1,list(range(1,2+1)))
+assert Dice(2,list(range(1,3+1))) != Dice(3,list(range(1,2+1)))
+assert Dice(1,list(range(1,2+1))).quant == 1
+assert Dice(1,list(range(1,2+1))).min() == [1]
+assert Dice(1,list(range(1,2+1))).max() == [2]
+assert Dice(1,list(range(1,2+1))).avg() == [1.5]
+assert repr(Dice(1,list(range(1,2+1)))) == 'Dice(1, [1, 2])'
+assert str(Dice(1,list(range(1,2+1)))) == '+1d2'
 
-assert Dice(2,2,True).min() == [-2,-2]
-assert Dice(2,2,True).max() == [-1,-1]
-assert Dice(2,2,True).avg() == [-1.5,-1.5]
-assert repr(Dice(1,2,True)) == 'Dice(1,2,True)'
-assert str(Dice(1,2,True)) == '-1d2'
+assert Dice(2,list(range(-2,-1+1))).min() == [-2,-2]
+assert Dice(2,list(range(-2,-1+1))).max() == [-1,-1]
+assert Dice(2,list(range(-2,-1+1))).avg() == [-1.5,-1.5]
+assert repr(Dice(1,list(range(-2,-1+1)))) == 'Dice(1, [-2, -1])'
+assert str(Dice(1,list(range(-2,-1+1)))) == '-1d2'
+
+assert Dice(2,[1,0,-1]).min() == [-1,-1]
+assert Dice(2,[1,0,-1]).max() == [1,1]
+assert Dice(2,[1,0,-1]).avg() == [0,0]
+assert repr(Dice(2,[1,0,-1])) == 'Dice(2, [1, 0, -1])'
+assert str(Dice(2,[1,0,-1])) == '+2df'
+
+assert Dice(3,[1,2,4,8]).min() == [1,1,1]
+assert Dice(3,[1,2,4,8]).max() == [8,8,8]
+assert Dice(3,[1,2,4,8]).avg() == [3.75,3.75,3.75]
+assert repr(Dice(3,[1,2,4,8])) == 'Dice(3, [1, 2, 4, 8])'
+assert str(Dice(3,[1,2,4,8])) == '+3d{1,2,4,8}'
